bac4483db9defca3f40c589e0ee35c3c0a321d06
Sebastián Salazar <juan-salazar@live.com>
2020-10-31 14:25:27 -0500

    upload work

---
+++ script.js
@@ -0,0 +1,12 @@
+function a() {
+    document.getElementById('bannerneu').src = 'resources/undraw_typewriter_i8xd.svg';
+
+}
+
+function b() {
+    document.getElementById('bannerneu').src = 'resources/undraw_studying_s3l7 (1).svg';
+}
+
+function c() {
+    document.getElementById('bannerneu').src = 'resources/undraw_professor_8lrt.svg';
+}
