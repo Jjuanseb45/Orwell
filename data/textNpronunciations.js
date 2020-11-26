82003f06db6b7bcac291053c395723199e6eba70
Sebastián Salazar <juan-salazar@live.com>
2020-11-26 05:14:29 -0500

    Adding more speaking

--- data/textNpronunciations.js
+++ data/textNpronunciations.js
@@ -14,6 +14,10 @@ let dataPronunciations = [
     new textNpronunciation(1, 'Library', 'Laibrari'),
     new textNpronunciation(2, 'How are you?', '¿Jau ar iu?'),
     new textNpronunciation(2, 'Am fine, thank you', 'Am, fain thenkiu'),
+    new textNpronunciation(2, 'May I go to the bathroom please?', 'Mei ai gou tude bath rum plis?'),
+    new textNpronunciation(3, 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?', 'Jau mach wud wuld a wudchock chock if a wudchock culd chock wud'),
+    new textNpronunciation(3, 'Black background, brown background', 'Blac bacgraund, braun bacgraund'),
+    new textNpronunciation(3, 'I scream, you scream they screan, we all scream for an ince cream!', 'Ai scrim, iu scrim, dei creim, weol scrim for an ais crim')
 ];
 
 
